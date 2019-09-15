9bd75535b5055bd86b359d580492af48800b7425
markli <chenxili@Chenxis-MacBook-Pro.local>
2019-09-15 01:08:46 -0400

    ..

--- tweets.sh
+++ tweets.sh
@@ -2,7 +2,7 @@
 export GOOGLE_APPLICATION_CREDENTIALS=tweetAnalyzer-965deefe97e3.json
 source env/bin/activate
 # if have not installed google natual language or tweepy yet
-sudo pip install --upgrade google-natural-language
+sudo pip install --upgrade google-cloud-language
 sudo pip install tweepy
 
 python tweets.py 
